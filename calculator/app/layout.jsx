5cfb2dbd4a950bd017e4e04cb3e7dc3932b50ded
avvicky <avvickya@gmail.com>
2023-07-30 13:31:43 +0530

    level1 task3 initial commit

---
+++ calculator/app/layout.jsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Calculator",
+  description: "Builded for codsoft intership",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width,
+                        initial-scale=1, shrink-to-fit=no"
+        />
+      </head>
+      <body>
+        <div id="wrapper">
+          <div id="app">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
